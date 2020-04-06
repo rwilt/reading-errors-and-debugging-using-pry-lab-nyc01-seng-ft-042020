d5781eb17d08c5083ba63d847314e44d31b8399c
rosie w <rosie.wilt@gmail.com>
2020-04-06 21:15:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,7 +1,8 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def get_user_input
-  gets.chomp
+  num = gets.chomp
+
 end
 
 def prompt_user
@@ -11,14 +12,17 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+
+ if num == 2
+    puts  "HAM HAM HAM IN MY TUMMY"
+
   end
 end
 
 def runner
   prompt_user
   selection(get_user_input)
+
 end
+    binding.pry
+runner
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,12 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    10.times do
+      "s" + string
+    end
   else
     string
   end
 end
+
+snake_it_up
